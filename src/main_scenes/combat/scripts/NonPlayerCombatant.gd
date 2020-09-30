62cd8d6555d0113c7e224f33eafe997c379aef2a
dkdawkins <ddawkins417@gmail.com>
2020-09-30 14:56:56 -0500

    Bug Fix
    -Fixed an issue where non-player characters would act before the turn queue was ready to recieve the turn_finished signal

--- src/main_scenes/combat/scripts/NonPlayerCombatant.gd
+++ src/main_scenes/combat/scripts/NonPlayerCombatant.gd
@@ -4,11 +4,17 @@ const PC = preload("res://main_scenes/combat/scripts/PlayerCombatant.gd")
 
 func set_active(isCombatantActive):
 	active = isCombatantActive
-	if active:
-		#Target and attack the first PC found
-		var target
-		for combatant in get_parent().get_children():
-			if combatant is PC:
-				target = combatant
-				break
-		perform_action("attack", target)
+	if not active:
+		return
+	$ActionDelay.start()
+	yield($ActionDelay, "timeout")
+	#Target and attack the first PC found
+	var target
+	for combatant in get_parent().get_children():
+		if combatant is PC:
+			target = combatant
+			break
+	perform_action("attack", target)
+
+##	FOR TESTING ONLY
+#	perform_action("attack", get_parent().get_node("PlayerCombatant"))
