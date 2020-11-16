055936207bf463feab2df04e7b9406a2997ae9ae
dkdawkins <ddawkins417@gmail.com>
2020-11-16 16:21:22 -0600

    Selectable Objects
    -The direction the player is facing is now tracked by the KinematicBody2d
    -Players can now interact with objects in front of them by pressing 'Z'.

--- src/main_scenes/exploration/scripts/Player.gd
+++ src/main_scenes/exploration/scripts/Player.gd
@@ -1,17 +1,20 @@
 extends KinematicBody2D
 
+enum Facing {LEFT, RIGHT, UP, DOWN}
+
 export var speed = 5.0
 export var tileSize = 64.000
 
 onready var sprite = $AnimatedSprite
 
-var direction = Vector2() #(0,0), (1,0), (0,1)
+var direction = Vector2() #(0,0), (1,0), (-1,0), (0,1), (0,-1)
 var counter = 0.0
-
+var facing = Facing.DOWN
 var moving = false
 
 
 func _process(delta):
+	get_action()
 	if not moving:
 		set_direction()
 	elif direction != Vector2():
@@ -21,8 +24,9 @@ func _process(delta):
 
 func set_direction():
 	direction = get_direction()
-	animate(direction)
-	if direction.x != 0 or direction.y != 0:
+	animate()
+	if direction != Vector2():
+		set_facing()
 		moving = true
 
 func get_direction():
@@ -34,6 +38,32 @@ func get_direction():
 		y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	return Vector2(x, y)
 
+func set_facing():
+	if direction.x > 0:
+		facing = Facing.RIGHT
+	elif direction.x < 0:
+		facing = Facing.LEFT
+	elif direction.y > 0:
+		facing = Facing.DOWN
+	else:
+		facing = Facing.UP
+
+func get_action():
+	if Input.is_action_just_pressed("ui_select"):
+		var collision = null
+		#Check if there is an object/obstacle in front of the player
+		match facing:
+			Facing.LEFT:
+				collision = move_and_collide((Vector2(-1,0)*tileSize), true, true, true)
+			Facing.RIGHT:
+				collision = move_and_collide((Vector2(1,0)*tileSize), true, true, true)
+			Facing.UP:
+				collision = move_and_collide((Vector2(0,-1)*tileSize), true, true, true)
+			_:
+				collision = move_and_collide((Vector2(0,1)*tileSize), true, true, true)
+		if collision != null:
+			print(collision.collider.name)
+
 func move(delta):
 	counter += delta * speed
 	move_and_collide((direction*tileSize*(delta*speed)))
@@ -43,8 +73,8 @@ func move(delta):
 		if get_direction() != direction:
 			moving = false
 
-func animate(direction):
-	if direction.x != 0 or direction.y != 0:
+func animate():
+	if direction != Vector2():
 		if direction.x > 0:
 			sprite.flip_h = true
 			sprite.play("walking_h")
